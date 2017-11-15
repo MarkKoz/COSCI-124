e09332e296088d5567c4663bd32aa0b6141703a2
MarkKoz <KozlovMark@gmail.com>
2017-11-14 16:55:45 -0800

    Changed exercise 11 to use a regular expression instead.

--- src/Exercise 11.py
+++ src/Exercise 11.py
@@ -1,18 +1,13 @@
-from typing import List
-from itertools import groupby
+import re
 
 inp: str = input("Enter a string: ")
 
-# Splits by upper-case characters. Converts iterator to a string with join().
-groups: List[str] = [''.join(g[1]) for g in groupby(inp, str.isupper)]
+# Finds all matches of the pattern. The pattern matches any uppercase character
+# followed by zero or more of any character other than an uppercase one.
+# Concatenates the matches into a space-delimited string.
+out: str = " ".join(re.findall(r"[A-Z][^A-Z]*", inp))
 
-# Merges every two groups in the list into one string. Done by zipping two
-# subsets from groups which contain every second element, one starting at index
-# 0 and the other at index 1.
-# Then concatenates every word into a space delimited string.
-out: str = ' '.join(''.join(c) for c in zip(groups[0::2], groups[1::2]))
-
-# Ensures only the first character is capitalised.
-out = out[0].upper() + out[1:].lower()
+# # Ensures only the first character is capitalised.
+out = out[0] + out[1:].lower()
 
 print(out)
