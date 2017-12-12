78fd4a76ca36cad31c01e24198232b223f95a8c7
MarkKoz <KozlovMark@gmail.com>
2017-12-12 13:41:16 -0800

    Documented exercise 10-2.

--- src/Exercise 10-02.py
+++ src/Exercise 10-02.py
@@ -1,7 +1,9 @@
-#
+# A class representing a car, which takes a make and model year.
+# Has functions simulating acceleration and braking, which modify a field that
+# keeps track of the car's speed.
 class Car:
     def __init__(self, year_model, make):
-        self.__year_model = year_model;
+        self.__year_model = year_model # Removed trailing semicolon.
         self.__make = make
         self.__speed = 0
 
@@ -9,31 +11,37 @@ class Car:
         self.__speed += 5
 
     def brake(self):
-        self.__speed -=5
+        self.__speed -= 5
 
     def get_speed(self):
         return self.__speed
 
 
-#
-import car
+# Not needed since the class is in the same module as main().
+#import car
 
 def main():
-    #
-    my_car = car.Car('2008', 'Honda Accord')
+    # Removed module qualifier for same reason as removing the import.
+    # Constructs and retrieves an instance to a Car class with the given
+    # arguments.
+    my_car = Car('2008', 'Honda Accord')
 
-    #
+    # Calls the accelerate function on the car instance 5 times, bringing the
+    # total speed to 25. Prints the current speed for every iteration of the
+    # loop i.e. the new speed after each acceleration.
     print('car is accelerating: ')
     for i in range(5):
         my_car.accelerate()
-        print ('Current speed: ', my_car.get_speed())
+        print('Current speed: ', my_car.get_speed())
     print()
 
-    #
-    print ('car is braking: ')
+    # Calls the brake function on the car instance 5 time, bringing the total
+    # speed to 0. Prints the current speed for every iteration of the loop i.e.
+    # the new speed after each brake.
+    print('car is braking: ')
     for i in range(5):
         my_car.brake()
-        print ('Current speed: ', my_car.get_speed())
+        print('Current speed: ', my_car.get_speed())
 
-#
+# Calls the main function when the program runs.
 main()
