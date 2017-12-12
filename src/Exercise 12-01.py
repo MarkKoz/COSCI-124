03c52acd824b4bc82ec10f25d74538a6e92d0d53
MarkKoz <KozlovMark@gmail.com>
2017-12-12 13:56:02 -0800

    Documented exercise 12-1.

--- src/Exercise 12-01.py
+++ src/Exercise 12-01.py
@@ -1,18 +1,20 @@
 def main():
- #
- number = 0
+    # Redundantly declares and initialises the variable to an "empty" value.
+    number = 0
 
- #
- number = int(input('How many numbers to display? '))
+    # Variable is set to something useful - the user's input.
+    number = int(input('How many numbers to display? '))
 
- #
- print_num(number)
+    # Calls the print_num function with the user's input as the argument.
+    print_num(number)
 
-#
+# Recursively calls itself while the argument is greater than 1. One is
+# subtracted from the argument for every recursive call. The result is numbers
+# printed in ascending order up to the value of the argument (inclusive).
 def print_num(n):
     if n > 1:
         print_num(n - 1)
-    print (n, sep=' ')
+    print(n, sep = ' ')
 
-#
+# Calls the main function when the program runs.
 main()
