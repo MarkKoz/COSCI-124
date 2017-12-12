64d3589b107fe82cd342ed78ca89eb39d0e79d82
MarkKoz <KozlovMark@gmail.com>
2017-12-12 14:04:44 -0800

    Documented exercise 12-2.

--- src/Exercise 12-02.py
+++ src/Exercise 12-02.py
@@ -1,25 +1,29 @@
 def main():
-    #
+    # Declares and initialises variables.
     num1 = 0
     num2 = 0
 
-    #
+    # Retrieves user input until a number greater than 0 is entered.
     while num1 <= 0:
         num1 = int(input('Enter the first number: '))
 
-    #
+    # Retrieves user input until a number greater than 0 is entered.
     while num2 <= 0:
         num2 = int(input('Enter the second number: '))
 
-    #
+    # Prints the result of the multiply function with the two user inputs as
+    # its arguments.
     print(num1, 'times', num2, 'is', multiply(num1, num2))
 
-#
+# Returns the product of multiplying the two arguments.
+# Works recursively, returning the multiplicand plus the return value of the
+# recursive call with the multiplier decremented by one. This effectively
+# adds the multiplicand for multiplier number of times.
 def multiply(x, y):
     if x == 0 or y == 0:
         return 0
     else:
         return x + multiply(x, y - 1)
 
-#
+# Calls the main function when the program runs.
 main()
